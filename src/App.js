8bdf1ab3a2dc815fb76fed9025144bb1dd0b6673
didoogan <tzd0409@gmail.com>
2024-01-03 16:53:52 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and by cool like Tymoshenko Dmytro :)
         </a>
       </header>
     </div>
